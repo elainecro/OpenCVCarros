6b40d7eb135ab972b0b4ae40a9b9558b4001f107
ElaineCRO <elainecro@gmail.com>
2020-02-25 11:15:22 -0300

    Adequando reconhecedor de placas

--- 2-ReconhecedorPlacas.py
+++ 2-ReconhecedorPlacas.py
@@ -2,9 +2,7 @@
 # import libraries of python OpenCV  
 import cv2 
   
-# capture frames from a video 
-video = cv2.VideoCapture('videos\\20200220.mp4') 
-image = cv2.imread("imagens\\carros1.png")
+image = cv2.imread("imagens\\carros4.png")
   
 # Trained XML classifiers describes some features of some object we want to detect 
 #car_cascade = cv2.CascadeClassifier('xml\\cars.xml') 
@@ -74,29 +72,6 @@ def computeSafeRegion(shape,bounding_rect):
         right = max_right
     return [left,top,right-left,bottom-top]   
   
-# loop runs if capturing has been initialized. 
-while video.isOpened(): 
-    # reads frames from a video 
-    ret, frames = video.read() 
-      
-    # convert to gray scale of each frames 
-    gray = cv2.cvtColor(frames, cv2.COLOR_BGR2GRAY)       
-  
-    # Detects cars of different sizes in the input image 
-    cars = car_cascade.detectMultiScale(gray, 1.1, 1) 
-      
-    # To draw a rectangle in each cars 
-    for (x,y,w,h) in cars: 
-        cv2.rectangle(frames,(x,y),(x+w,y+h),(0,0,255),2) 
-  
-    # Display frames in a window  
-    #cv2.imshow('video2', frames) 
+
     
-    images = detectPlateRough(frames,frames.shape[0],top_bottom_padding_rate=0.1)
-      
-    # Wait for Esc key to stop 
-    if cv2.waitKey(33) == 27: 
-        break
-  
-# De-allocate any associated memory usage 
-cv2.destroyAllWindows() 
+images = detectPlateRough(image,image.shape[0],top_bottom_padding_rate=0.1)
